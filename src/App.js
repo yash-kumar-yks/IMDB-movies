22a6d21c13539cef47d8882267b67a655cbdc6c9
yash <yashsngh57@gmail.com>
2021-11-16 14:03:08 +0530

    data fetching

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { NavBar, MovieList } from "./components";
+import axios from "axios";
+
+const API_BASE_URL = "https://www.omdbapi.com";
+// &apikey=aa660442
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  /**
+   *
+   * function search(){
+   * }
+   */
+  const search = async (e) => {
+    if (e.code === "Enter") {
+      // https://www.omdbapi.com/?s=avatar&apikey=aa660442
+      setIsLoading(true);
+      const response = await axios.get(
+        API_BASE_URL + "/?s=" + inputValue + "&apikey=aa660442"
+      );
+      // set data
+      setMovies(response.data.Search);
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <NavBar
+        search={search}
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+      />
+      <MovieList isLoading={isLoading} movieList={movies} />
+      {/* {count} -> 1 */}
+      {/* MovieList */}
     </div>
   );
 }
