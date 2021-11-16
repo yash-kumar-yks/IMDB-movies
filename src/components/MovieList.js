955151b113ef59c74889a2288b3864627c93c56f
yash <yashsngh57@gmail.com>
2021-11-16 14:04:38 +0530

    movie list

---
+++ src/components/MovieList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "../styles/movie-list.css";
+import MovieCard from "./MovieCard";
+
+export default function MovieList({ movieList, isLoading }) {
+  if (isLoading) {
+    return "Loading Movies please wait!";
+  } else {
+    return (
+      <div className="movie-list">
+        {movieList.map((movie) => (
+          <MovieCard movie={movie} key={movie.imdbID} />
+        ))}
+      </div>
+    );
+  }
+}
